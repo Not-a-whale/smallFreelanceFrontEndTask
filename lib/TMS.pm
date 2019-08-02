76ddbb731d767b847eaad53b125a9a9bbefa6ca9
Vlad Klimkiv <vladklimkiv@uskoinc.com>
2019-08-02 13:24:31 -0700

    turned on JSON serializer for dancer

--- lib/TMS.pm
+++ lib/TMS.pm
@@ -1,10 +1,27 @@
 package TMS;
 use Dancer2;
+use Dancer2::Logger::File;
+use strict;
+use warnings;
+use Data::Dumper;
+use Try::Tiny;
 
-our $VERSION = '0.1';
+set session => 'Simple';
+set engines => {
+    serializer => {
+        JSON => {
+            allow_nonref => 1
+        }
+    }
+};
+
+set serializer => 'JSON';
+set content_type => 'application/json';
+
+our $VERSION = '3.1';
 
 get '/' => sub {
-    template 'index' => { 'title' => 'TMS' };
+    return "hello";
 };
 
 true;
