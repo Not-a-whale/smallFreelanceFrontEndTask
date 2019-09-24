359289efed88504c2f39e63c3c10415471fe0e4f
Vlad Klimkiv <vladklimkiv@uskoinc.com>
2019-09-24 14:21:13 -0700

    updated selects

--- public/modules/generic/select/module.js
+++ public/modules/generic/select/module.js
@@ -1,36 +1,112 @@
 class GenericSelectCtrl extends GenericInputCtrl {
-  constructor(getid, $http) {
-    super(getid);
+  constructor(getid, scope, http) {
+    super(getid, scope);
 
-    this.$http = $http;
+    this.http = http;
     this.options = [];
+    this.optionsname = 'list';
+    this.valname = 'id';
+    this.keyname = 'name';
+    this.groupname = 'group';
+    this.disablename = 'disabled';
   }
 
-  $onInit() {
-    var self = this;
-    if (self.sample) {
-      self.$http.get('/modules/temp/select_data.json').then(function (res) {
-        self.options = res.data;
-        self.value = self.options[0];
-      }, function (res) {
-        console.log("Class: Error loading sample values")
-      });
-    } else {
-      self.$http.get(self.url).then(function (res) {
-        self.options = res.data;
-        if (self.default) {
-          self.value = self.options[0];
-        }
+  Reset() {
+    this.FindSelected(this.value);
+  }
 
-      }, function (res) {
-        console.log("Could not load data for element #" + self.id);
+  Update() {
+    if (this.showValue != this.value && this.onUpdate instanceof Function) {
+      this.onUpdate({
+        value: this.showValue,
+        id: this.showValue[this.valname]
       });
     }
   }
+
+  FindSelected(val) {
+    if (val == undefined) {
+      this.showValue = undefined;
+      return;
+    }
+    var self = this;
+    this.options.forEach((x) => {
+      if (val == x[self.valname]) {
+        self.showValue = x;
+        return;
+      }
+    });
+  }
+
+  FetchOptions() {
+    var self = this;
+    if (this.url) {
+      this.http.get(this.url).then(
+        function (res) {
+          if (!('DATA' in res.data)) {
+            console.error("Malformed response for select " + self.id);
+            return;
+          }
+          if (!(self.optionsname in res.data.DATA)) {
+            console.error("Malformed response for select " + self.id +
+              "\nNo " + self.optionsname + " in response");
+            return;
+          }
+          // checks if data has supplied valname inside
+          if (res.data.DATA[self.optionsname].length > 0) {
+            if (!(self.valname in res.data.DATA[self.optionsname][0])) {
+              console.error("Malformed response data for select " + self.id +
+                "\nNo " + self.valname + " in repsonse data");
+              return;
+            }
+            if (!(self.keyname in res.data.DATA[self.optionsname][0])) {
+              console.error("Malformed response data for select " + self.id +
+                "\nNo " + self.keyname + " in repsonse data");
+              return;
+            }
+          }
+          self.options = res.data.DATA[self.optionsname];
+          if (self.default != undefined) {
+            self.FindSelected(self.default);
+          }
+
+        },
+        function (res) {
+          console.error("Failed to fetch data for select " + self.id);
+        });
+    } else {
+      if (this.options == undefined) {
+        console.error("No url provided for select " + self.id);
+      }
+    }
+  }
+
+  $onInit() {
+    super.$onInit();
+    this.FetchOptions();
+  }
 }
+
+var select_bindings = {
+  value: '<?',
+  options: '<?', // list of options to bind when needed
+  label: '@?',
+  errmsg: '@?',
+  required: '@?',
+  disabled: '@?',
+  url: '@?',
+  optionsname: '@?', // the attribute/key to use to grab the list of options from response
+  keyname: '@?', // the attribute/key to use to display the options in the select
+  valname: '@?', // the attribute/key to use to select the option
+  disablename: '@?', // the attribute/key to use to determine if option is disabled
+  groupname: '@?', // the attribute/key to use to group options
+  onUpdate: '&?'
+};
+
+
 
 app.component('genericSelect', {
   templateUrl: '/modules/generic/select/default.html',
-  controller: ['getid', '$http', GenericSelectCtrl],
+  controller: ['getid', '$scope', '$http', GenericSelectCtrl],
   bindings: select_bindings
 });
