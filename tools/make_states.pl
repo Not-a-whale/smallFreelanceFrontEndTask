c3462a6391233cabdaff8e964ccac988271301c8
Vlad Klimkiv <vladklimkiv@uskoinc.com>
2019-11-11 11:47:51 -0800

    added state generator -- deprecated

--- tools/make_states.pl
+++ tools/make_states.pl
@@ -1,5 +1,10 @@
-#!/user/bin/perl
+#!/usr/bin/perl
 # $Id: $
+
+BEGIN {
+    unshift @INC,
+      './lib';    #having to do this is kind of annoying and reduces portability
+}
 
 use strict;
 use warnings FATAL => 'all';
@@ -8,159 +13,202 @@ use Data::Dumper;
 use Cwd qw(realpath);
 use File::Basename qw(dirname);
 use JSON;
+use TMS::StateBuilder;
 
-sub GenerateString {
-    my $minlength = shift;
-    my $variance  = shift;
-    my @chars     = ( 'a' .. 'z', 'A' .. 'Z', '0' .. '9' );
-    my $length    = int( rand(1) * $variance + $minlength );
-    return
-      join( '', map { $chars[ int( rand() * scalar @chars ) ] } 0 .. $length );
-}
+# my $rules = {
+#     dryrun => {
+#         isa     => 'Bool',
+#         alias   => 'd',
+#         comment => 'Dry run, prints every command that will be executed',
+#         default => 0
+#     },
+#     basedir => {
+#         isa   => 'Str',
+#         alias => 'b',
+#         comment =>
+# 'The directory to traverse, dirs -> statenames, .html -> view template'
+#     },
+#     statefn => {
+#         isa     => 'Str',
+#         alias   => 'n',
+#         comment => 'The filename of the state config',
+#         default => 'configstates.js'
+#     },
+#     statedir => {
+#         isa   => 'Str',
+#         alias => 's',
+#         comment =>
+#           'Where to place the state config file, overwrites existing file'
+#     },
+# };
+
+# my $CLI = ProcessCLI( rules => $rules, isfatal => 1 );
+
+# sub GenerateString {
+#     my $minlength = shift;
+#     my $variance  = shift;
+#     my @chars     = ( 'a' .. 'z', 'A' .. 'Z', '0' .. '9' );
+#     my $length    = int( rand(1) * $variance + $minlength );
+#     return
+#       join( '', map { $chars[ int( rand() * scalar @chars ) ] } 0 .. $length );
+# }
 
 my $rootdir = realpath( dirname( realpath($0) ) . '/../public' );
-my $basedir = "/views";
+my $basedir = "views";
 
-my $config_filename = "configstates.js";
+my $config_filename = "configstates_compiled.js";
 my $confdir         = "/views";
 
-my @dirs = ("");
-my %views;    # quick reference to view => states
+my $config = {
+    #dry      => undef,
+    rootdir  => "$rootdir/$basedir",
+    webdir   => $basedir,
+    statefn  => $config_filename,
+    statedir => "$rootdir$confdir",
+    #state => "tmsapp.main.tomato.dragon.fork.stick",
+};
 
-my %states;   # holds the states that will later be JSONified
-my %state_to_path;
+TMS::StateBuilder::BuildStatesFile($config);
+TMS::StateBuilder::CreateStateDirFiles($config);
 
-my @dependency;
+# my @dirs = ("");
+# my %views;    # quick reference to view => states
 
--d $rootdir || confess "Have to run from root directory of app.";
-my $pwd;
-do {
-    $pwd = shift @dirs;
+# my %states;   # holds the states that will later be JSONified
+# my %state_to_path;
 
-    # extract all dirs and files from current directory
-    my $opendir = "$rootdir$basedir$pwd";
-    opendir( DIR, $opendir ) || confess "Cannot open directory $opendir";
-    my @files = readdir(DIR);
-    closedir(DIR);
+# my @dependency;
 
-    foreach my $file (@files) {
+# -d $rootdir || confess "Have to run from root directory of app.";
+# my $pwd;
+# do {
+#     $pwd = shift @dirs;
 
-        # skip parent and current dirs
-        next if $file =~ /^\.\.?$/;
+#     # extract all dirs and files from current directory
+#     my $opendir = "$rootdir$basedir$pwd";
+#     opendir( DIR, $opendir ) || confess "Cannot open directory $opendir";
+#     my @files = readdir(DIR);
+#     closedir(DIR);
 
-        my $path = "$opendir/$file";
+#     foreach my $file (@files) {
 
-        #generate the state that will be used in angularjs
+#         # skip parent and current dirs
+#         next if $file =~ /^\.\.?$/;
 
-        my $state;
-        $state = $pwd if -f $path;    # files are used to link views
-        $state = "$pwd/$file"
-          if -d $path;                # directories are used for state names
-        $state =~ s/^\///;            # remove the first leading backslash
-        $state =~ s/\//./g;           # states use . to describe hierarchy
+#         my $path = "$opendir/$file";
 
-        if ( -d $path ) {
+#         #generate the state that will be used in angularjs
 
-            # already visted the state so skip it
-            next if exists $states{$state};
+#         my $state;
+#         $state = $pwd if -f $path;    # files are used to link views
+#         $state = "$pwd/$file"
+#           if -d $path;                # directories are used for state names
+#         $state =~ s/^\///;            # remove the first leading backslash
+#         $state =~ s/\//./g;           # states use . to describe hierarchy
 
-            # this will include the "empty non-named state needed for angularjs"
-            # the state has to be skipped when building the JSON and JS files
-            $states{$state} = {};
-            $states{$state}{"url"} = '/' . GenerateString( 4, 6 );
+#         if ( -d $path ) {
 
-            # map state to path for simplicity
-            $state_to_path{$state} = $path;
+#             # already visted the state so skip it
+#             next if exists $states{$state};
 
-            push @dirs, "$pwd/$file";
-            next;
-        }
+#             # this will include the "empty non-named state needed for angularjs"
+#             # the state has to be skipped when building the JSON and JS files
+#             $states{$state} = {};
+#             #$states{$state}{"url"} = '/' . GenerateString( 4, 6 );
+#             $states{$state}{"url"} = "/$file";
 
-        next if ( $file !~ /(.*)\.html$/i );   # only looking for template files
+#             # map state to path for simplicity
+#             $state_to_path{$state} = $path;
 
-        # check the name of the files
-        # template => then this is the template of the current state ui-view
-        # all other named files belong to named views
-        my $view = $1;
+#             push @dirs, "$pwd/$file";
+#             next;
+#         }
 
-        # find all the ui-view in the file if its named, then store it
-        open( my $fh, "<", $path ) || confess "Cannot open file $path";
-        {
-            local $/;
-            my $content = <$fh>;
-            foreach ( $content =~ /ui-view="([^"]+?)"/g ) {
-                $views{$_} = $state;
-            }
-        }
-        close($fh);
-        next if $state !~ /\w+/;
-        my $web_path = "$basedir$pwd/$file";
-        if ( $view =~ 'template' ) {
-            $states{$state}{"templateUrl"} = $web_path;
-        }
-        else {
-            if ( exists $views{$view} ) {
-                my $parent = $views{$view};
-                $states{$state}{views}{ $view . '@' . $parent }{"templateUrl"}
-                  = $web_path;
-            }
-            else {
-                push @dependency, [ $pwd, $file ];
-            }
-        }
-    }
-} while ( scalar @dirs );
+#         next if ( $file !~ /(.*)\.html$/i );   # only looking for template files
 
-# these are all the files that were not able to be linked to states
-# try to link again
-foreach (@dependency) {
-    my $pwd  = @$_[0];
-    my $file = @$_[1];
+#         # check the name of the files
+#         # template => then this is the template of the current state ui-view
+#         # all other named files belong to named views
+#         my $view = $1;
 
-    $file =~ /(.*)\.html$/i;
-    my $view = $1;
+#         # find all the ui-view in the file if its named, then store it
+#         open( my $fh, "<", $path ) || confess "Cannot open file $path";
+#         {
+#             local $/;
+#             my $content = <$fh>;
+#             foreach ( $content =~ /ui-view="([^"]+?)"/g ) {
+#                 $views{$_} = $state;
+#             }
+#         }
+#         close($fh);
+#         next if $state !~ /\w+/;
+#         my $web_path = "$basedir$pwd/$file";
+#         if ( $view =~ 'template' ) {
+#             $states{$state}{"templateUrl"} = $web_path;
+#         }
+#         else {
+#             if ( exists $views{$view} ) {
+#                 my $parent = $views{$view};
+#                 $states{$state}{views}{ $view . '@' . $parent }{"templateUrl"}
+#                   = $web_path;
+#             }
+#             else {
+#                 push @dependency, [ $pwd, $file ];
+#             }
+#         }
+#     }
+# } while ( scalar @dirs );
 
-    print "$view\n";
+# # these are all the files that were not able to be linked to states
+# # try to link again
+# foreach (@dependency) {
+#     my $pwd  = @$_[0];
+#     my $file = @$_[1];
 
-    my $web_path = "$basedir$pwd/$file";
+#     $file =~ /(.*)\.html$/i;
+#     my $view = $1;
 
-    my $state = $pwd;
-    $state =~ s/^\///;     # remove the first leading backslash
-    $state =~ s/\//./g;    # states use . to describe hierarchy
+#     print "$view\n";
 
-    if ( exists $views{$view} ) {
-        my $parent = $views{$view};
-        $states{$state}{views}{ $view . '@' . $parent }{"templateUrl"} =
-          $web_path;
-    }
-    else {
-        confess "Dependency view not found for $basedir$pwd/$file";
-    }
-}
+#     my $web_path = "$basedir$pwd/$file";
 
-print Dumper( \%views );
-#print Dumper( \%states );
-#print Dumper( \@dependency );
-print Dumper ( keys %states );
-print Dumper( \%state_to_path );
+#     my $state = $pwd;
+#     $state =~ s/^\///;     # remove the first leading backslash
+#     $state =~ s/\//./g;    # states use . to describe hierarchy
 
-if ( scalar @dependency ) {
-    confess "Could not build file because view dependency not satisfied"
-      . Dumper( \@dependency );
-}
+#     if ( exists $views{$view} ) {
+#         my $parent = $views{$view};
+#         $states{$state}{views}{ $view . '@' . $parent }{"templateUrl"} =
+#           $web_path;
+#     }
+#     else {
+#         confess "Dependency view not found for $basedir$pwd/$file";
+#     }
+# }
 
-my $json = JSON->new->allow_nonref->indent->canonical([1]);
+# print Dumper( \%views );
+# #print Dumper( \%states );
+# #print Dumper( \@dependency );
+# print Dumper ( keys %states );
+# print Dumper( \%state_to_path );
 
-open( my $fh, ">", "$rootdir$confdir/$config_filename" )
-  || confess "Could not open file to write configstates";
+# if ( scalar @dependency ) {
+#     confess "Could not build file because view dependency not satisfied"
+#       . Dumper( \@dependency );
+# }
 
-print $fh 'var configstates = ' . $json->encode( \%states );
-close($fh);
+# my $json = JSON->new->allow_nonref->indent->canonical([1]);
 
-foreach my $state (sort keys %states ) {
-    next if $state !~ /\w+/;
-    my $filename = $state_to_path{$state} . '/state.json';
-    open( my $fh, ">", $filename );
-    print $fh $json->encode( { $state => $states{$state} } );
-    close($fh);
-}
+# open( my $fh, ">", "$rootdir$confdir/$config_filename" )
+#   || confess "Could not open file to write configstates";
+
+# print $fh 'var configstates = ' . $json->encode( \%states );
+# close($fh);
+
+# foreach my $state (sort keys %states ) {
+#     next if $state !~ /\w+/;
+#     my $filename = $state_to_path{$state} . '/state.json';
+#     open( my $fh, ">", $filename );
+#     print $fh $json->encode( { $state => $states{$state} } );
+#     close($fh);
+# }
