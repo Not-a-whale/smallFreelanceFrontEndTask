e07b81e0f34710ae43d25bbba0c1364adf050837
Vlad Klimkiv <vladklimkiv@uskoinc.com>
2019-11-11 17:04:43 -0800

    created input to work with meta/data from dbtools

---
+++ public/modules/generic/input/meta.module.js
@@ -0,0 +1,22 @@
+class MetaInputCtrl {
+  constructor(getid) {
+    this.id = getid();
+  }
+}
+
+var meta_bindings = {
+  gate: '=', // this is the input but showValue is the one being changed
+  meta: '<', // this has the meta data
+};
+
+let meta_input_bindings = CloneObj(meta_bindings);
+meta_input_bindings['type'] = '@';
+
+
+app.controller('MetaInputCtrl', ['getid', MetaInputCtrl]);
+
+app.component('metaInput', {
+  templateUrl: 'modules/generic/input/default.html',
+  controller: 'MetaInputCtrl',
+  bindings: meta_input_bindings
+});
