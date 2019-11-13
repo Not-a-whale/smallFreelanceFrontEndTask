8c0d18089c585677c8e40578b1dddf6030301ea4
Vlad Klimkiv <vladklimkiv@uskoinc.com>
2019-11-12 16:15:14 -0800

    added customer business form component as per design

---
+++ public/modules/business/customer/form/meta.module.js
@@ -0,0 +1,143 @@
+class MetaBusinessCustomerFormCtrl {
+  constructor(http) {
+    this.http = http;
+    this.meta = {
+      "DUNS": {
+        "attr": {
+          "required": null,
+          "title": "DUNS",
+          "label": "DUNS"
+        },
+        "tree": {}
+      },
+      "CstmrId": {
+        "tree": {
+          "RootNode": {
+            "attr": {
+              "required": null,
+              "title": "RootNode",
+              "label": "RootNode"
+            },
+            "tree": {}
+          },
+          "BizId": {
+            "attr": {
+              "label": "BizId",
+              "required": null,
+              "title": "BizId"
+            },
+            "tree": {}
+          },
+          "BizURL": {
+            "attr": {
+              "label": "BizURL",
+              "title": "BizURL",
+              "required": null
+            },
+            "tree": {}
+          },
+          "BizName": {
+            "attr": {
+              "label": "BizName",
+              "required": "required",
+              "title": "BizName"
+            },
+            "tree": {}
+          }
+        },
+        "attr": {
+          "label": "CstmrId",
+          "title": "CstmrId",
+          "required": null
+        }
+      },
+      "DOT": {
+        "tree": {},
+        "attr": {
+          "label": "DOT",
+          "title": "DOT",
+          "required": null
+        }
+      },
+      "SCAC": {
+        "tree": {},
+        "attr": {
+          "required": null,
+          "title": "SCACC",
+          "label": "SCAC"
+        }
+      },
+      "Factoring": {
+        "attr": {
+          "title": "Factoring",
+          "required": null,
+          "label": "Factoring"
+        },
+        "tree": {}
+      },
+      "CreditLimit": {
+        "attr": {
+          "label": "CreditLimit",
+          "title": "Credit Limit",
+          "required": null
+        },
+        "tree": {}
+      },
+      "WhyDontUse": {
+        "attr": {
+          "label": "WhyDontUse",
+          "required": null,
+          "title": "Why do not use"
+        },
+        "tree": {}
+      },
+      "Bond": {
+        "tree": {},
+        "attr": {
+          "label": "Bond",
+          "title": "Bond",
+          "required": null
+        }
+      },
+      "DontUse": {
+        "tree": {},
+        "attr": {
+          "title": "Do not use",
+          "required": null,
+          "label": "DontUse"
+        }
+      },
+      "Terms": {
+        "attr": {
+          "title": "Payment terms",
+          "required": null,
+          "label": "Terms"
+        },
+        "tree": {}
+      },
+      "MC": {
+        "tree": {},
+        "attr": {
+          "required": null,
+          "title": "MC number",
+          "label": "MC"
+        }
+      }
+    };
+  }
+
+  // TODO: remove this
+  Post() {
+    var self = this;
+    this.http.post("/echo", this.gate).then(function (res) {
+      console.log(res.status);
+    });
+  }
+}
+
+app.controller('MetaBusinessCustomerFormCtrl', ['$http', MetaBusinessCustomerFormCtrl]);
+app.component('metaBusinessCustomerForm', {
+  templateUrl: 'modules/business/customer/form/meta.template.html',
+  controller: 'MetaBusinessCustomerFormCtrl',
+  bindings: meta_bindings
+});
