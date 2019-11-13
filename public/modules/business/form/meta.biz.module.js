96ebd62d8deda2bd01874b956f64258e149b23d7
Vlad Klimkiv <vladklimkiv@uskoinc.com>
2019-11-13 15:30:27 -0800

    removed default business form component to atomic business form component

--- public/modules/business/form/meta.biz.module.js
+++
@@ -1,6 +0,0 @@
-app.controller('MetaBusinessFormCtrl', MetaBusinessFormCtrl);
-app.component('metaBizForm', {
-  templateUrl: 'modules/business/form/meta.biz.template.html',
-  controller: 'MetaBusinessFormCtrl',
-  bindings: meta_bindings
-});
