a6f1fc91790fbaa59d7074aabe17bab11148891b
Peter Chuk <cpeter@uskoinc.com>
2019-11-18 16:46:29 -0800

    Preliminary Feature Test

---
+++ lib/TMS/API/Feature/CntAddress.pm
@@ -0,0 +1,40 @@
+package TMS::API::Feature::CntAddress;
+
+use strict;
+use warnings FATAL => 'all';
+use Moose;
+
+extends 'TMS::API::Core::CntAddress';
+
+my $TREE = {
+    AddrId  => {required => 0,},
+    Street1 => {required => 1,},
+    Street2 => {required => 0,},
+    Street3 => {required => 0,},
+    City    => {required => 1,},
+    Zip     => {required => 1,},
+    State   => {required => 1,},
+    Country => {required => 0,},
+    GpsLng  => {required => 0,},
+    GpsLat  => {required => 0,},
+    Notes   => {required => 0,},
+};
+
+has "+Street1" => (required => 0,);
+has "+City"    => (required => 0,);
+has "+Zip"     => (required => 0,);
+has "+State"   => (required => 0,);
+
+sub Sifter {
+    my $posts = shift;
+    my $flour = {};
+    $posts = shift if ref($posts) ne 'HASH';
+    foreach (keys %$TREE) {
+        $$flour{$_} = $$posts{$_} if exists $$posts{$_}  && defined $$posts{$_};
+#        $$flour{$_} = undef       if !exists $$flour{$_} && $$TREE{$_}{required};
+    }
+    return $flour;
+} ## end sub Sifter
+
+1;
+
