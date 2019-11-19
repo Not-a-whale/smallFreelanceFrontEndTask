4817e8be03849d37a8dff3d1bc69ab1fceda3c2b
Vlad Klimkiv <vladklimkiv@uskoinc.com>
2019-11-19 14:28:42 -0800

    changed phones features to return a comma delimited string

--- public/modules/personnel/phone/features/meta.module.js
+++ public/modules/personnel/phone/features/meta.module.js
@@ -39,6 +39,18 @@ class MetaPhoneFeaturesInputCtrl extends MetaGateCtrl{
         "tree": {}
       }
     };
+
+    this.values = {};
+  }
+
+  UpdateOutput(){
+    let vals = [];
+    for (let v in this.values){
+      if (this.values[v] == true){
+        vals.push(v);
+      }
+    }
+    this.gate = vals.join(',');
   }
 }
 
