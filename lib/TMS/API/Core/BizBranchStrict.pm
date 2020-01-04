87879216873e45c822bd361e0598ff6b8e2c431d
Peter Chuk <cpeter@uskoinc.com>
2020-01-03 16:16:12 -0800

    FK are not required

--- lib/TMS/API/Core/BizBranchStrict.pm
+++ lib/TMS/API/Core/BizBranchStrict.pm
@@ -1,9 +1,9 @@
 package TMS::API::Core::BizBranchStrict;
 use Moose::Role;
 
-has 'BizId'        => ('is' => 'rw', 'isa' => 'PositiveInt',      'coerce' => '1', 'required' => '1');
-has 'BrnchAddress' => ('is' => 'rw', 'isa' => 'PositiveInt',      'coerce' => '1', 'required' => '1');
-has 'BrnchPhone'   => ('is' => 'rw', 'isa' => 'PositiveInt',      'coerce' => '1', 'required' => '1');
-has 'OfficeName'   => ('is' => 'rw', 'isa' => 'TidySpacesString', 'coerce' => '1', 'required' => '1', 'default' => '');
+has 'BizId'        => ('is' => 'rw', 'isa' => 'PositiveInt', 'coerce' => '1', 'required' => '0');
+has 'BrnchAddress' => ('is' => 'rw', 'isa' => 'PositiveInt', 'coerce' => '1', 'required' => '0');
+has 'BrnchPhone'   => ('is' => 'rw', 'isa' => 'PositiveInt', 'coerce' => '1', 'required' => '0');
+has 'OfficeName'   => ('is' => 'rw', 'isa' => 'TidySpacesString',  'coerce' => '1', 'required' => '1', 'default' => '');
 
 1;
