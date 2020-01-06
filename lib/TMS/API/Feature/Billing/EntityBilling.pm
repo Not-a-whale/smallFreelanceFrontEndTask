44569c350f955909c31c904e70dba3d6bdd0770c
Peter Chuk <cpeter@uskoinc.com>
2020-01-06 12:31:27 -0800

    beautification

--- lib/TMS/API/Feature/Billing/EntityBilling.pm
+++ lib/TMS/API/Feature/Billing/EntityBilling.pm
@@ -2,17 +2,15 @@ package TMS::API::Feature::Billing::EntityBilling;
 
 use Moose;
 
-use TMS::API::Core::FinBillingInfo;
+use TMS::API::Core::Entity;
 use TMS::API::Core::FinTaxId;
 use TMS::API::Core::EntPerson;
 use TMS::API::Core::BizBranch;
-use TMS::API::Core::CntPhonesfax;
 use TMS::API::Core::CntAddress;
 use TMS::API::Core::EntBusiness;
-use TMS::API::Core::EntPerson;
-
-use TMS::API::Core::Entity;
+use TMS::API::Core::CntPhonesfax;
 use TMS::API::Joins::EntityBilling;
+use TMS::API::Core::FinBillingInfo;
 
 extends 'TMS::API::Feature::Features';
 
