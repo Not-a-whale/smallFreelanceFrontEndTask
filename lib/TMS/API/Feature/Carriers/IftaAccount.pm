fc5f38cbc3765cccaaba301c7debfee3d6b3b418
Peter Chuk <cpeter@uskoinc.com>
2020-01-08 12:58:14 -0800

    cosmetic changes, removed empty lines

--- lib/TMS/API/Feature/Carriers/IftaAccount.pm
+++ lib/TMS/API/Feature/Carriers/IftaAccount.pm
@@ -6,4 +6,3 @@ extends 'TMS::API::Feature::Features';
 
 sub _build_coreapi  { shift->coreapi('TMS::API::Core::CrrIfta') }
 sub _build_prefetch { shift->prefetch(TMS::API::Joins::CrrIfta->new->prefetch) }
-
