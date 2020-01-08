3b370b344885243e023a5e7f661adb814d9c62ca
Peter Chuk <cpeter@uskoinc.com>
2020-01-08 11:02:59 -0800

    IFTA Account feature

---
+++ lib/TMS/API/Feature/Carriers/IftaAccount.pm
@@ -0,0 +1,9 @@
+package TMS::API::Feature::Carriers::IftaAccount;
+use Moose;
+use TMS::API::Core::CrrIfta;
+use TMS::API::Joins::CrrIfta;
+extends 'TMS::API::Feature::Features';
+
+sub _build_coreapi  { shift->coreapi('TMS::API::Core::CrrIfta') }
+sub _build_prefetch { shift->prefetch(TMS::API::Joins::CrrIfta->new->prefetch) }
+
