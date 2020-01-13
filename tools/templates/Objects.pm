b00d25cd02046480250abd314d458dd69547eee1
Peter Chuk <cpeter@uskoinc.com>
2020-01-13 11:40:50 -0800

    Auto Generate

--- tools/templates/Objects.pm
+++ tools/templates/Objects.pm
@@ -7,14 +7,17 @@ use namespace::autoclean;
 use Devel::Confess;
 use Data::Dumper;
 
-$Data::Dumper::Terse = 1;
-
 use Moose;
 use Moose::Util::TypeConstraints;
 use TMS::API::Types::Tools;
 
+our $AUTO_GENERATE = 0;
+$Data::Dumper::Terse = 1;
+
+sub _auto_ok { $AUTO_GENERATE && (!defined $_[0] || $_[0] !~ /\S+/s) ? 1 : 0 }
+
 sub _build_type {
-    my ($class,$input) = @_;
+    my ($class, $input) = @_;
     my $trait = $class . TMS::API::Types::Tools::Chained();
     return $class->with_traits($trait)->new(%$input);
 }
