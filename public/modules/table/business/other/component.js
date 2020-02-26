ad23abd8722c0606c90fac7445adf94d2610396a
Vlad Klimkiv <vladklimkiv@uskoinc.com>
2020-02-26 12:50:27 -0800

    refactored other business table to use table core class

--- public/modules/table/business/other/component.js
+++ public/modules/table/business/other/component.js
@@ -1,41 +1,46 @@
-class BusinessTableCtrl {
-  constructor(scope, businesssrv, state, chipsrv, timeout) {
-    this.scope = scope;
-    this.timeout = timeout;
-    // used to focus the input fields, need to delay after search expands
-    this.focuscall = undefined;
+class BusinessOtherTableCtrl extends TableCoreCtrl {
+  constructor(scope, state, chipsrv, timeout, businesssrv) {
+    super(scope, state, chipsrv, timeout);
 
-    this.chipsrv = chipsrv;
-    this.state = state;
     this.businesssrv = businesssrv;
     this.fields = [{
       name: 'name',
       size: 'c3'
     }, {
-      name: 'mc',
+      name: 'type',
       size: 'c2'
     }, {
-      name: 'address',
+      name: 'branch address',
       size: 'c4'
     }, {
-      name: 'amount insured',
+      name: 'primary contact',
       size: 'c3'
+    }, {
+      name: 'company phone',
+      size: 'c2'
+    }, {
+      name: 'status',
+      size: 'c2'
     }];
 
 
 
     this.map = {
       'name': ['biz.BizName', 'OfficeName'],
+      'type': ['TODO'],
+      'primary contact': ['TODO'],
+      'company phone': ['TODO'],
+      'status': ['TODO'],
       'mc': ['biz.has_carrier.MC'],
-      'address': ['brnch_address.Street1', 'brnch_address.City', 'brnch_address.State', 'brnch_address.Zip', 'brnch_address.Country'],
+      'branch address': ['brnch_address.Street1', 'brnch_address.City', 'brnch_address.State', 'brnch_address.Zip', 'brnch_address.Country'],
       'amount insured': ['TODO'],
       'business name': ['biz.BizName'],
       'office name': ['OfficeName'],
-      'street': ['brnch_address.Street1','brnch_address.Street2','brnch_address.Street3'],
+      'street': ['brnch_address.Street1', 'brnch_address.Street2', 'brnch_address.Street3'],
       'city': ['brnch_address.City'],
       'state': ['brnch_address.State'],
       'zip': ['brnch_address.Zip'],
-      'dot' : ['biz.has_carrier.DOT'],
+      'dot': ['biz.has_carrier.DOT'],
       'scac': ['biz.has_carrier.SCAC']
     };
   }
@@ -48,54 +53,20 @@ class BusinessTableCtrl {
     });
   }
 
-  PrepSearch(){
-    let args = {
-      fields: this.chipsrv.GetChips(),
-      sort: this.chipsrv.GetSort()
-    };
-
-    console.log('broadcasting TableSearch with args');
-    this.scope.$broadcast('TableSearch', args);
-  }
-
-  UpdateSort(field, value){
-    this.chipsrv.SetSort(field, value);
-    this.chipsrv.SetOrder(field);
-    this.PrepSearch();
-  }
-
-  UpdateField(field, value){
-    this.chipsrv.AddChip(field, value);
-    this.PrepSearch();
-  }
-
-  Select(item){
+  Select(item) {
+    return undefined;
     let id = item.BizId;
     this.state.go('tmsapp.main.hr.biz.carrier.list.detail', {
       'id': id
     });
   }
 
-  FocusMe(index){
-    let self = this;
-    let args = {
-      index: index
-    };
-    this.scope.$broadcast('SearchOpen');
-    this.timeout( function(){
-      self.scope.$broadcast('SearchFocus', args);
-    }, 100);
-  }
 
-  $onInit(){
-    this.chipsrv.Reset();
-    this.chipsrv.SetMap(this.map);
-  }
 }
 
-app.component('carrierTable', {
-  templateUrl: 'modules/tables/carrier/template.html',
-  controller: ['$scope', 'BusinessService', '$state', 'TableSortChips', '$timeout', CarrierTableCtrl],
+app.component('tableBusinessOther', {
+  templateUrl: 'modules/table/business/other/template.html',
+  controller: ['$scope', '$state', 'TableSortChips', '$timeout', 'BusinessService', BusinessOtherTableCtrl],
   bindings: {
     carrierList: '<?'
   }
