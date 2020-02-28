18eab34916d5b6839c405dbab7557d187f2738bd
Vlad Klimkiv <vladklimkiv@uskoinc.com>
2020-02-28 07:37:46 -0800

    fixed height and width problems with ui-card-detail-small

--- public/modules/ui/card/detail/small/module.js
+++ public/modules/ui/card/detail/small/module.js
@@ -1,19 +1,5 @@
 class UICardDetailSmallCtrl {
   constructor() {
-
-    this.address = {
-      "City": "CONSEQUATUR. LABO",
-      "AddrId": 766602425,
-      "Notes": "Reprehenderit et iste et. Impedit sit illo laudantium corrupti. Quas maxime dolore id odio facilis.",
-      "Zip": "16584-9731",
-      "Street1": "CUM DOLOR MINIMA HARUM TENETUR ENIM LABORE EA ILLUM.",
-      "GpsLat": 7045.03,
-      "State": "AK",
-      "Country": "United States",
-      "GpsLng": 8103.42,
-      "Street2": "FACERE EST VOLUPTATIBUS.",
-      "Street3": "PERFERENDIS ET BLANDITIIS."
-    };
   }
 
   HasGroup() {
