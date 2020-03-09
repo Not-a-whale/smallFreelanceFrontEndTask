2a62911438383c54c4070a8f9498afc7ce4336e5
Peter Chuk <cpeter@uskoinc.com>
2020-03-09 13:10:10 -0700

    DB Schema is changed, removing

--- lib/TMS/API/Core/AppPermissionSearch.pm
+++
@@ -1,6 +0,0 @@
-package TMS::API::Core::AppPermissionSearch;
-use Moose::Role;
-
-has 'AccessName' => ('is' => 'rw', 'isa' => 'TidySpacesString', 'coerce' => '1', 'required' => '0');
-
-1;
--- lib/TMS/API/Core/AppPermissionStrict.pm
+++
@@ -1,6 +0,0 @@
-package TMS::API::Core::AppPermissionStrict;
-use Moose::Role;
-
-has 'AccessName' => ('is' => 'rw', 'isa' => 'TidySpacesString', 'coerce' => '1', 'required' => '1');
-
-1;
