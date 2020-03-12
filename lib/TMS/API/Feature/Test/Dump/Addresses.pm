421ec5d52e25f9e50879f12970c01b44278edf6b
Peter Chuk <cpeter@uskoinc.com>
2020-03-12 11:32:51 -0700

    Basics to see raw data

---
+++ lib/TMS/API/Feature/Test/Dump/Addresses.pm
@@ -0,0 +1,26 @@
+package TMS::API::Feature::Test::Dump::Addresses;
+use Moose;
+
+use TMS::API::Core::CntAddress;
+
+extends 'TMS::API::Feature::Features';
+
+has coreapi => (
+    is       => 'ro',
+    required => 1,
+    isa      => 'Str',
+    init_arg => undef,
+    default  => 'TMS::API::Core::CntAddress'
+);
+has prefetch => (
+    is         => 'rw',
+    required   => 0,
+    isa        => 'Undef|HashRef|ArrayRef',
+    lazy_build => 1
+);
+
+sub _build_prefetch {
+    shift->prefetch(undef);
+}
+
+1;
