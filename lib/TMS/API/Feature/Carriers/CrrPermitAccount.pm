281e066f09d012d5eab0c2c022d027c5a4b83496
Peter Chuk <cpeter@uskoinc.com>
2020-03-12 11:15:50 -0700

    Depricated feature

--- lib/TMS/API/Feature/Carriers/CrrPermitAccount.pm
+++
@@ -1,8 +0,0 @@
-package TMS::API::Feature::Carriers::CrrPermitAccount;
-use Moose;
-use TMS::API::Core::CrrPermitAccount;
-use TMS::API::Joins::CrrPermitAccount;
-extends 'TMS::API::Feature::Features';
-
-sub _build_coreapi  { shift->coreapi('TMS::API::Core::CrrPermitAccount') }
-sub _build_prefetch { shift->prefetch(TMS::API::Joins::CrrPermitAccount->new->prefetch) }
