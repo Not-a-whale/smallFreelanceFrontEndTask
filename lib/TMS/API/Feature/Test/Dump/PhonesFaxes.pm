a2f3dd979a572148acd3144296b32a0d6e6855fd
Peter Chuk <cpeter@uskoinc.com>
2020-03-12 11:36:45 -0700

    Basics to see raw data

---
+++ lib/TMS/API/Feature/Test/Dump/PhonesFaxes.pm
@@ -0,0 +1,26 @@
+package TMS::API::Feature::Test::Dump::PhonesFaxes;
+use Moose;
+
+use TMS::API::Core::CntPhonesfax;
+
+extends 'TMS::API::Feature::Features';
+
+has coreapi => (
+    is       => 'ro',
+    required => 1,
+    isa      => 'Str',
+    init_arg => undef,
+    default  => 'TMS::API::Core::CntPhonesfax'
+);
+has prefetch => (
+    is         => 'rw',
+    required   => 0,
+    isa        => 'Undef|HashRef|ArrayRef',
+    lazy_build => 1
+);
+
+sub _build_prefetch {
+    shift->prefetch(undef);
+}
+
+1;
