4d5ffaf3d180130e2cce178b2bdcdf0e96850d61
Vlad Klimkiv <vladklimkiv@uskoinc.com>
2020-03-16 16:31:28 -0700

    refactored person contact component

--- public/modules/ui/contact/person/component.js
+++ public/modules/ui/contact/person/component.js
@@ -12,38 +12,74 @@ class UIContactPersonCtrl extends UIContactCtrl {
     return this.image;
   }
 
+  ValidData() {
+    let valid = false;
+    if (this.data != undefined && !angular.equals(this.data, {})) {
+      if ('ast' in this.data) {
+        valid = true;
+      }
+    }
+    return valid;
+  }
+
   DisplayName() {
     return !this.hideName;
   }
 
   Name() {
     let value = 'no name provided';
-    if ('Person' in this.data) {
-      if ('FirstName' in this.data.Person && 'LastName' in this.data.Person) {
-        value = this.data.Person.FirstName + ' ' + this.data.Person.LastName;
+    if ('ast' in this.data) {
+      if ('FirstName' in this.data.ast && 'LastName' in this.data.ast) {
+        value = this.data.ast.FirstName + ' ' + this.data.ast.LastName;
       }
+    }
+    return value;
+  }
 
+  Role() {
+    let value = undefined;
+    if ('CurrentTitle' in this.data) {
+      value = this.data.CurrentTitle;
     }
     return value;
   }
 
   Address() {
     let value = undefined;
-    if ('Address' in this.data) {
-      value = this.data.Address;
+    if ('ast' in this.data) {
+      value = this.data.ast.brnch.brnch_address;
     }
     return value;
   }
 
-  ValidData() {
-    let valid = false;
-    if (this.data != undefined && !angular.equals(this.data, {})) {
-      if ('Person' in this.data) {
-        valid = true;
-      }
+  Group() {
+    let value = undefined;
+    if ('ast' in this.data) {
+      value = this.data.ast.brnch.OfficeName;
     }
-    return valid;
+    return value;
   }
+
+  Phone() {
+    let value = undefined;
+    if ('biz_phone' in this.data) {
+      value = this.data.biz_phone;
+    }
+    return value;
+  }
+
+
+  Email() {
+    let value = undefined;
+    if ('BizEmail' in this.data) {
+      if (this.data.BizEmail != undefined && this.data.BizEmail != ' ') {
+        value = this.data.BizEmail;
+      }
+
+    }
+    return value;
+  }
+
 }
 
 app.component('uiContactPerson', {
