3c3da573f6fe3d5db7642f55b2635bb276510362
Vlad Klimkiv <vladklimkiv@uskoinc.com>
2020-03-16 16:34:23 -0700

    refactored holds accordion container

--- public/modules/ui/container/accordion/holds/component.js
+++ public/modules/ui/container/accordion/holds/component.js
@@ -1,5 +1,12 @@
-app.component('uiHoldsAccordion', {
+class UIAccordionContainerHoldsCtrl {
+  constructor() {
+    this.title = 'Expirations and Holds';
+  }
+}
+
+app.component('uiAccordionHolds', {
   templateUrl: 'modules/ui/container/accordion/holds/template.html',
+  controller: UIAccordionContainerHoldsCtrl,
   bindings: {
     holds: '<?'
   }
