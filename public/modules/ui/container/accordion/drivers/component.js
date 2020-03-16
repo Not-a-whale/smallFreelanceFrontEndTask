48efbc44149094888c2ec789f37a43eba7709986
Vlad Klimkiv <vladklimkiv@uskoinc.com>
2020-03-16 16:33:53 -0700

    refactored driver accordion container

--- public/modules/ui/container/accordion/drivers/component.js
+++ public/modules/ui/container/accordion/drivers/component.js
@@ -1,5 +1,25 @@
-app.component('uiDriversAccordion', {
+class UIAccordionContainerDriverCtrl {
+  constructor() {
+    this.title = 'Drivers';
+    this.header = [{
+      name: 'name',
+      size: 'c4'
+    }, {
+      name: 'phone',
+      size: 'c3'
+    }, {
+      name: 'status',
+      size: 'c3'
+    }, {
+      name: 'holds',
+      size: 'c2'
+    }, ];
+  }
+}
+
+app.component('uiAccordionDrivers', {
   templateUrl: 'modules/ui/container/accordion/drivers/template.html',
+  controller: UIAccordionContainerDriverCtrl,
   bindings: {
     drivers: '<?'
   }
