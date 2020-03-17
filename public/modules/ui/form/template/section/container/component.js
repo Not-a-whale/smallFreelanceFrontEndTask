28144e2067f6350f97067c51c30499128c3c5e44
Vlad Klimkiv <vladklimkiv@uskoinc.com>
2020-03-17 08:30:26 -0700

    container for holding section items for modular forms

---
+++ public/modules/ui/form/template/section/container/component.js
@@ -0,0 +1,40 @@
+class UIFormSectionContainerCtrl {
+  constructor(formsrv) {
+    this.formsrv = formsrv;
+    this.sections = undefined;
+  }
+
+  $onInit() {
+    this.sections = this.formsrv.CurrentSubform().forms;
+  }
+
+  EnabledSections() {
+    if (this.sections != undefined) {
+      return this.sections.filter(x => x.active);
+    }
+
+  }
+
+  DisabledSections() {
+    if (this.sections != undefined) {
+      return this.sections.filter(x => !x.active);
+    }
+  }
+
+  ScrollToSection(section) {
+    if (this.toScroll instanceof Function) {
+      this.toScroll({
+        section: section
+      });
+    }
+  }
+}
+
+
+app.component('uiFormSectionContainer', {
+  templateUrl: 'modules/ui/form/template/section/container/template.html',
+  controller: ['FormService', UIFormSectionContainerCtrl],
+  bindings: {
+    toScroll: '&?'
+  }
+});
