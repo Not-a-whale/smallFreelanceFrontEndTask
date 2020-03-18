96b37925cf77fb3a6b350a93f0f08dc9c20921b2
Vlad Klimkiv <vladklimkiv@uskoinc.com>
2020-03-18 12:42:33 -0700

    removed deprecated selector table template

--- public/modules/generic/table/selector/meta.module.js
+++
@@ -1,6 +0,0 @@
-app.component("metaTableSelector", {
-  controller: "MetaTableCtrl",
-  controllerAs: "tctrl",
-  templateUrl: "modules/generic/table/selector/meta.template.html",
-  bindings: meta_table_bindings
-});
--- public/modules/generic/table/selector/module.js
+++
@@ -1,6 +0,0 @@
-app.component("tableSelector", {
-  controller: "GenericTableCtrl",
-  controllerAs: "tctrl",
-  templateUrl: "modules/generic/table/selector/default.html",
-  bindings: table_bindings
-});
