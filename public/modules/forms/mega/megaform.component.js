3be277302f54b4d7f16b15ae905c936ee0b6bed2
Vlad Klimkiv <vladklimkiv@uskoinc.com>
2020-04-30 12:14:18 -0700

    added observer pattern to megaform & form-sections

--- public/modules/forms/mega/megaform.component.js
+++ public/modules/forms/mega/megaform.component.js
@@ -1,6 +1,40 @@
 class UIMegaFormCtrl {
   constructor(APIService) {
     this.api = APIService;
+    this.sections = [];
+  }
+
+  Register(ctrl) {
+    this.sections.push(ctrl);
+  }
+
+  /**
+   *
+   * @param {Object} depobj
+   * configuration object for dependencies
+   * {
+   *  section: string or array of strings
+   *  fields: array of object {from: string, to: string, type: const or data}
+   *  operator: or / and
+   * }
+   */
+  GetDependency(depobj) {
+    let depname = depobj.section;
+    let dep = undefined;
+    if (typeof depname === 'string') {
+      depname = [depname];
+    }
+    if (Array.isArray(depname)) {
+      for (let name of depname) {
+        // find first valid dependency and return the controller
+        dep = this.sections.find(x => x.type == name);
+        if (dep != undefined) {
+          break;
+        }
+      }
+
+    }
+    return dep;
   }
 }
 
